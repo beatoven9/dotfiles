f6c4b0a012a0efe5d83ba39ce835b5fc7e3563b0
laon <reeedt@gmail.com>
2021-05-08 23:53:35 -0400

    update 5-9-21

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+function makeLinks() {
+    ln -sfn ~/dotfiles/.tmux.conf ~/.tmux.conf
+    ln -sfn ~/dotfiles/.vimrc ~/.vimrc
+    ln -sfn ~/dotfiles/.Xmodmap ~/.Xmodmap 
+    ln -sfn ~/dotfiles/.zshrc ~/.zshrc
+    ln -sfn ~/dotfiles/.zsh_aliases ~/.zsh_aliases
+
+    ln -sfn ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+    ln -sfn ~/dotfiles/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+    ln -sfn ~/dotfiles/.config/i3/config ~/.config/i3/config
+
+
+    tmux source-file ~/.tmux.conf
+    xmodmap ~/.Xmodmap;
+}
